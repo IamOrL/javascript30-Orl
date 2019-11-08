d1fcedf7badc452e3a701306bb667af7cfd48621
Aurelien Gremy <agremy@solutionsmedias360.com>
2019-11-08 17:44:54 -0500

    d1 - modif listener 'load'

--- day-one-RomainDrumScript/js/drumScript.js
+++ day-one-RomainDrumScript/js/drumScript.js
@@ -1,5 +1,7 @@
+window.addEventListener('load', (event) => {
 
-window.addEventListener('keydown', function(event) {
+
+function playSound(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   if(!audio) return;
@@ -7,6 +9,7 @@ window.addEventListener('keydown', function(event) {
   audio.play();
   
   key.classList.add('playing');
+}
 
 function removeTransition(event) {
   if (event.propertyName !== 'transform') return;
@@ -14,10 +17,8 @@ function removeTransition(event) {
   this.classList.remove('playing');
 }
 
-
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
 });
-
-
-
