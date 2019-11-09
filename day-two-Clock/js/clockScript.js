404999c6b590c9e5d51be8f5a34316b686d0ad98
IamOrL <gremy.aurelien@gmail.com>
2019-11-09 15:01:38 -0500

    d2 - add the listener 'load'

--- day-two-Clock/js/clockScript.js
+++ day-two-Clock/js/clockScript.js
@@ -1,30 +1,28 @@
+window.addEventListener('load', (event) => {
 
-// const secondHand = document.querySelector('.second-hand');
-
-// console.log(secondHand);
-// console.log(document.querySelector('.second-hand'));
-// document.querySelector('.second-hand');
-
-//------
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
 
  function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsTime = ((seconds / 60) * 360) + 90;
-    document.querySelector('.second-hand').style.transform = `rotate(${secondsTime}deg)`;
+    secondHand.style.transform = `rotate(${secondsTime}deg)`;
     console.log(seconds);
 
     const mins = now.getMinutes();
     const minsTime = ((mins / 60) * 60) + 90;
-    document.querySelector('.min-hand').style.transform = `rotate(${minsTime}deg)`;
+    minsHand.style.transform = `rotate(${minsTime}deg)`;
     console.log(mins);
 
     const hours = now.getHours();
     const hoursTime = ((hours / 24) * 60) + 90;
-    document.querySelector('.hour-hand').style.transform = `rotate(${hoursTime}deg)`;
+    hoursHand.style.transform = `rotate(${hoursTime}deg)`;
     console.log(hours);
  }
 
  setInterval(setDate, 1000);
 
  setDate();
+});
