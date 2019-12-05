a3a4ef7ee46e6893a283ea27b9df11b9cd4f9214
Aurelien Gremy <agremy@solutionsmedias360.com>
2019-12-04 19:22:31 -0500

    day8 - in progress

--- day8-eight-Play-with-Canvas/js/playWithCanvas.js
+++ day8-eight-Play-with-Canvas/js/playWithCanvas.js
@@ -1,41 +1,20 @@
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+window.addEventListener('load', (event) => {
+    const canvas = document.querySelector('#draw');
+    const ctx = canvas.getContext('2d');
 
-const cities = [];
+    canvas.widht = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => cities.push(...data))
-    
-function findMatches(wordToMatch, cities) {
-    return cities.filter(place => {
-    
-    const regex = new RegExp(wordToMatch, 'gi');
-    return place.city.match(regex) || place.state.match(regex)
-    });
-}
+    ctx.strokeStyle = '#BADA55';
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
 
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
+    let isDrawing = false;
+    let lastX = 0;
+    let lastY = 0;
 
-function displayMatches() {
-    const matchArray = findMatches(this.value, cities);
-    const html = matchArray.map(place => {
-        const regex = new RegExp(this.value, 'gi');
-        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
-        return `
-            <li>
-                <span class="name">${cityName}, ${stateName}</span>
-                <span class="population">${numberWithCommas(place.population)}</span>
-            </li>
-        `;
-    }).join('');
-    suggestions.innerHTML = html;
-}
+    function draw(event) {
 
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
+    }
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+});
